64a879fe6d2b1d30f0a65c7c51be6a9f0da2b54e
Rajat Goyal <31059368+rajatgoyall@users.noreply.github.com>
2019-07-18 07:36:26 +0530

    Update ZeroMattrix_DP.cpp

--- DP/ZeroMattrix_DP.cpp
+++ DP/ZeroMattrix_DP.cpp
@@ -26,8 +26,10 @@ int getmaxzeros(int**input,int m,int n)
 	for(int i=1;i<m;i++)
 	{
 		for(int j=1;j<n;j++)
-		{
+		{if(input[i][j]==0)
 			dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+            else
+                dp[i][j]=0;
 		}
 	}
 	/*for(int i=0;i<m;i++)
